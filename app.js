4df9940f80f82530fc8d8d536e8ca4d621ad66d4
Jitendra Rajpoot <devansh.forlife@gmail.com>
2019-11-09 15:47:21 +0530

    Added More Routes

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const app = express();
 
 //Static Asset Middleware
 app.use(express.static(path.join(__dirname, "public")));
+
+//ROUTES Middlewares
+//Index Route
+app.use("/", require("./routes/welcome/welcome"));
+//Login Page Route
+app.use("/login", require("./routes/login/login"));
+//Register Page Route
+app.use("/register", require("./routes/register/register"));
+//Developer Page Route
+app.use("/developer", require("./routes/developer"));
+//Dashboard Page Route
+app.use("/dashboard", require("./routes/dashboard"));
+
 //Express-Handlebars Middleware
 app.engine(
   "handlebars",
@@ -14,9 +27,7 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 //Routes
-app.get("/", (req, res) => {
-  res.render("index");
-});
+
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log(`Server Is Up On The Port ${port}`);

bc910888dd7500f748d54d2ef8d5edb6d02829b8
Devforlife07 <Devansh.forlife@gmail.com>
2020-06-29 18:57:50 +0530

    Ready For Production

--- config/keys/keys.js
+++ config/keys/keys.js
@@ -1,9 +1,7 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://Devansh:Pass@123@cluster0-erq7v.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://Devansh:Pass@123@cluster0-erq7v.mongodb.net/test?retryWrites=true&w=majority",
   google: {
-    clientID:
-      "88884088884-st85sqijfpafom7hm1444dt5edb70osm.apps.googleusercontent.com",
+    clientID: "88884088884-st85sqijfpafom7hm1444dt5edb70osm.apps.googleusercontent.com",
     clientSecret: "zw7uDmjEG91YagBeQP-7yjeP"
   }
 };
